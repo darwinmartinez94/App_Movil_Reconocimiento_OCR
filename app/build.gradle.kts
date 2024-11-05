51db70a731a10921f502af88f1e77bcaa7358e1f
darwinmartinez94 <130948314+darwinmartinez94@users.noreply.github.com>
2024-11-05 13:28:47 -0600

    Actualizacion de GaleryFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // To recognize Latin script
+    implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.1")
 }
--- app/src/main/java/com/uth/reconocimiento_ocr/ui/galery/GaleryFragment.java
+++ app/src/main/java/com/uth/reconocimiento_ocr/ui/galery/GaleryFragment.java
@@ -1,20 +1,30 @@
 package com.uth.reconocimiento_ocr.ui.galery;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 import com.uth.reconocimiento_ocr.databinding.FragmentGaleryBinding;
+
+import java.io.InputStream;
 
 public class GaleryFragment extends Fragment {
 
     private FragmentGaleryBinding binding;
+    //variable para buscar la imagen desde drawable
+    private Bitmap imagenSeleccionada;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,9 +34,32 @@ public class GaleryFragment extends Fragment {
         binding = FragmentGaleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //final TextView textView = binding.textNotifications;
-        //galeryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //llamar a los metodos para el OCR
+        imagenSeleccionada = obtenerImagendeAsset(this.getContext(), "identidad.png");
+
+
         return root;
+    }
+
+    //METODO PARA EJECUTRAR EL RECONOCEDOR DE OCR
+    private void ejecutarReconocedorOCR(){
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        InputImage imageEnPantall = InputImage.fromBitmap(imagenSeleccionada, 0);
+
+    }
+
+    //Metdo para buscar una imagen en mi carpeta drawable
+    private static Bitmap obtenerImagendeAsset(Context contexto, String nombreArchivo){
+        AssetManager assetManager = contexto.getAssets();
+        InputStream is;
+        Bitmap bitmap = null;
+        try {
+            is = assetManager.open(nombreArchivo);
+            bitmap = BitmapFactory.decodeStream(is);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return bitmap;
     }
 
     @Override
